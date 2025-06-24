57f2d44f4ad0b0206f7eeba4349b30c1674db39e
Theo Ford <theo.e.ford@gmail.com>
2025-06-24 11:23:44 +0100

    tailwind 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,12 @@ export default async function Home() {
   const home = await client.getByUID("page", "home");
 
   // <SliceZone> renders the page's slices.
-  return <SliceZone slices={home.data.slices} components={components} />;
+  return (
+    <>
+      <p className="text-[200px] text-red-800">TEST</p>
+      <SliceZone slices={home.data.slices} components={components} />
+    </>
+  );
 }
 
 export async function generateMetadata(): Promise<Metadata> {
