8039b39a949a3eb4dadbd1fc8dfaf2bd7882568b
Theo Ford <theo.e.ford@gmail.com>
2025-06-24 11:39:57 +0100

    tailwind 6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
